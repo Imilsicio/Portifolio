936817cffa216d2ba4f8a853e634fc46d4f1605d
Imilsicio Monteiro <imilsiciomonteiro@gmail.com>
2024-08-15 16:44:30 +0200

    Teste alteracao

--- app/work/page.jsx
+++ app/work/page.jsx
@@ -1,4 +1,23 @@
-import React from 'react'
+"use client";
+
+import {motion} from "framer-motion";
+import {Swiper, SwiperSlide} from "swiper/react"
+import React, {useState} from 'react';
+import "swiper/css";
+import { BsArrow90DegRight, BsGithub } from "react-icons/bs";
+import { Tooltip, TooltipContent, TooltipProvider,TooltipTrigger } from "@radix-ui/react-tooltip";
+
+import Link from "next/link";
+import Image from "next/image";
+
+const project = [
+  {
+    num:'01',
+    category:'frontend',
+    title: 'project 1',
+    description: ''
+  }
+]
 
 const work = () => {
   return (
